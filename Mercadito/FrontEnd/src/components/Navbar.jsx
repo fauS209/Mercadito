11a0369890ce175be2f14050c0751b69519ce405
Fauricio salazar vega <fausalazar10@gmail.com>
2024-11-26 20:13:26 -0600

    Start FrontEnd
    Se da inicio al front

---
+++ Mercadito/FrontEnd/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router'
+import './Navbar.css'
+export const Navbar = () => {
+  return (
+    
+    <div >
+    
+        <p>
+        <Link to={"/Home"}> Home</Link>
+        </p>
+        <p>
+        <Link to={"/App"}>App </Link>
+        </p>
+        <p>
+        <Link to={"/Contact"}>Contact</Link>
+        </p>
+        <p>
+        <Link to={"/Dashbord"}>Dashboard</Link>
+        </p>
+
+    </div>
+    
+  )
+}
+export default Navbar;
---
+++ Mercadito/FrontEnd/src/routes/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+export const Dashbord = () => {
+  return (
+
+    <div>
+      <Navbar/>
+      <h1>Dashbord</h1>
+    
+    </div>
+  )
+}
+export default Dashbord;
