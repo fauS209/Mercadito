25ac36c252caedb3347bf658d7032456dd03bb15
Fauricio salazar vega <fausalazar10@gmail.com>
2024-11-28 19:02:48 -0600

    login

--- Mercadito/FrontEnd/src/routes/Login.jsx
+++ Mercadito/FrontEnd/src/routes/Login.jsx
@@ -1,13 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./css/Login.css"
 import Navbar from '../components/Navbar';
-
+import axios from 'axios';
 export const Login = () => {
-return (
-    <div>     
-        <Navbar/>
-    <h1>Login</h1>
 
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    async function login(event) {
+        event.preventDefault();
+        await axios.post("http://localhost:8080/api/v1/mercadito/users/login",{
+        email:email,
+        password: password,
+        }).then((res)=>{
+            console.log(res.data);
+
+            if(res.data.message == "Email not exists"){
+                alert("Email not exists")
+            }
+            else if(res.data.message == "Bienvenido"){
+                alert("Bienvenido")
+                
+            }
+
+
+        }
+    )
+        
+    }
+
+return (
+
+
+    <div className="container">
+        <Navbar/>
+    <h2 className="header">Inicio de Sesión</h2>
+    <form className="form">
+      {/* Campo de correo electrónico */}
+    <div className="input-group">
+        <label htmlFor="email" className="label">
+        Correo Electrónico:
+        </label>
+        <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            className="input"
+            value={email}
+            onChange={(event)=>{
+            setEmail(event.target.value)
+        }}
+        />
+        
     </div>
+
+      {/* Campo de contraseña */}
+    <div className="input-group">
+        <label htmlFor="password" className="label">
+        Contraseña:
+        </label>
+        <input
+        type="password"
+        id="password"
+        name="password"
+        required
+        className="input"
+        value={password}
+        onChange={(event)=>{
+        setPassword(event.target.value)
+    }}
+        />
+    </div>
+
+      {/* Botón de inicio de sesión */}
+    <button
+    type="submit"
+    className="button"
+    onClick={login}
+    >
+        Iniciar Sesión
+    </button>
+    </form>
+</div>
     )
 }
 export default Login;
--- Mercadito/FrontEnd/src/routes/Register.jsx
+++ Mercadito/FrontEnd/src/routes/Register.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
+
 import Navbar from '../components/Navbar';
 
 export const Register = () => {
@@ -12,17 +12,3 @@ return (
     )
 }
 export default Register;
-=======
-import Navbar from '../components/Navbar'
-
-const Register = () => {
-  return (
-    <div>
-        <Navbar/>
-        <h1>Register</h1>
-    </div>
-  )
-}
-
-export default Register
->>>>>>> 2578df7441d7db6fc6236096b3b72aa7f8baf2ac
