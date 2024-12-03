342ad7ed16cb01635f1f567510d913f8570846f7
Potoy <potoy128@gmail.com>
2024-12-03 13:43:22 -0600

    App page design improvements

--- Mercadito/FrontEnd/src/App.jsx
+++ Mercadito/FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import mesaMadera from './assets/mesa-madera.jpg'
+import logoMercadito from './assets/logoMercadito.png';
 import './App.css'
 import navbar, { Navbar } from './components/Navbar'
 import { Link } from 'react-router'
@@ -14,32 +15,19 @@ function App() {
       <div className="app-container">
         <Navbar />
         <h1>El Mercadito Tigreño</h1>
-        <div class="card"  >
-          <div class="card-body">
-            <h5 class="card-title">Bienvenido a su mercadito de confianza</h5>
-            
-            <img src='/logoMercadito.png' alt='logo' className='logo-mercadito' />
+        <div className="card"  >
+          <div className="card-body">
+            <h5 className="card-title">Bienvenido a su mercadito de confianza</h5>
+            <div className='loguito'>
+              <img src= "/logoMercadito.png" alt="loguito"  className='card-image'/>
+            </div>
             <div className='card-text'>
-              <p class="card-text">“Productos frescos y de calidad, apoyando a nuestros productores locales para que disfrutes de una compra confiable y responsable.”</p>
+              <p>“Productos frescos y de calidad, apoyando a nuestros productores locales para que disfrutes de una compra confiable y responsable.”</p>
             </div>
-            {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+            <Link to={'/Register'} className="btn">Registrate</Link> 
           </div>
-          <Link to={'/Register'} className='btn'> Registrate</Link>
+          {/* <Link to={'/Register'} className='btn'> Registrate</Link> */}
         </div>
-        {/* <div className='botoncito'>
-        <Link to={'/Register'} className='btn'> Click</Link>
-        </div> */}
-        {/* <footer className="footer">
-          <div className="footer-content">
-            <p>&copy; 2024 El Mercadito Tigreño. Todos los derechos reservados.</p>
-            <p>Contacto: <a href="mailto:info@mercaditotigreño.com">info@mercaditotigreño.com</a></p>
-            <div className="footer-links">
-              <Link to="/about" className="footer-link">Acerca de</Link>
-              <Link to="/privacy" className="footer-link">Política de Privacidad</Link>
-              <Link to="/terms" className="footer-link">Términos de Servicio</Link>
-            </div>
-          </div>
-        </footer> */}
 
       </div>
     </>
